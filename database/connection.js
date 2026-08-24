eae63d3a619b2ad51932c4aa0c182328b7b31388
juanjo <juanjoseatuestaflor@gmail.com>
2026-08-24 18:29:59 -0500

    agregamos un Dockerfile y docker-compose para desplegar la aplicacion en contenedores

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+
 const connection = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/autogestionLubritecaDB");
+    const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/autogestionLubritecaDB";
+    await mongoose.connect(MONGO_URI);
     console.log("Conexión a la base de datos establecida correctamente");
   } catch (error) {
     console.error("Error al conectar a la base de datos: ", error);
